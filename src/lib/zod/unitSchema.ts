2d5a6eefeb9304405d48b6d6446e557be0a43a7f
Mirza Ali Abhipraya <mirzaaliabhipraya@gmail.com>
2025-11-28 13:20:04 +0700

    Feat: Add and fix type schema for Vehicle and Accessories

--- src/lib/zod/unitSchema.ts
+++ src/lib/zod/unitSchema.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 
 export const unitVehicle = z.object({
+  _id: z.string().optional(),
   name: z.string().min(1, { message: "Format nama kendaraan tidak valid" }),
   brand: z.string().min(1, { message: "Format brand kendaraan tidak valid" }),
   plateNumber: z.string().min(1, { message: "Format plat nomor tidak valid" }),
@@ -23,7 +24,13 @@ export const unitVehicle = z.object({
   vehicleType: z.enum(["Car", "Bike"]),
 });
 
+export const unitVehicleArray = z.array(unitVehicle).refine((arr) => {
+  if (arr.length === 0) return true;
+  return true;
+});
+
 export const unitAccessories = z.object({
+  _id: z.string().optional(),
   name: z.string().min(1, { message: "Format nama aksesoris tidak valid" }),
   brand: z.string().min(1, { message: "Format brand aksesoris tidak valid" }),
   amount: z.number().min(1, { message: "Format jumlah aksesoris tidak valid" }),
@@ -46,5 +53,39 @@ export const unitAccessories = z.object({
   }),
 });
 
+export const unitAccessoriesArray = z.array(unitAccessories).refine((arr) => {
+  if (arr.length === 0) return true;
+  return true;
+});
+
+export const unitVehicleEdit = z.object({
+  _id: z.string().optional(),
+  name: z.string().min(1, { message: "Format nama kendaraan tidak valid" }),
+  brand: z.string().min(1, { message: "Format brand kendaraan tidak valid" }),
+  plateNumber: z.string().min(1, { message: "Format plat nomor tidak valid" }),
+  plateCity: z.string().min(1, { message: "Format plat nomor tidak valid" }),
+  plateArea: z.string().min(1, { message: "Format plat nomor tidak valid" }),
+  year: z.number().min(1, { message: "Format tahun kendaraan tidak valid" }),
+  price: z.object({
+    perHour: z
+      .number()
+      .min(1, { message: "Format harga kendaraan perjam tidak valid" }),
+    perDay: z
+      .number()
+      .min(1, { message: "Format harga kendaraan perhari tidak valid" }),
+    perWeek: z
+      .number()
+      .min(1, { message: "Format harga kendaraan perminggu tidak valid" }),
+    perMonth: z
+      .number()
+      .min(1, { message: "Format harga kendaraan perbulan tidak valid" }),
+  }),
+  vehicleStatus: z.enum(["Ready", "Rented", "Maintenance"]),
+  vehicleType: z.enum(["Car", "Bike"]),
+});
+
 export type unitVehicleType = z.infer<typeof unitVehicle>;
+export type unitVehicleEditType = z.infer<typeof unitVehicleEdit>;
 export type unitAccessoriesType = z.infer<typeof unitAccessories>;
+export type unitVehicleArrayType = z.infer<typeof unitVehicleArray>;
+export type unitAccessoriesArrayType = z.infer<typeof unitAccessoriesArray>;
